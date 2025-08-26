4936df31efc21b2006a1657806264ec35fe1b2a7
tulsibhagat259 <riya.bhagat.259@gmail.com>
2025-08-26 22:17:48 +0530

    daily

---
+++ assiment/Lab/SQUARE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	float side,area;
+	printf("\n Enter the Length of side : ");
+	scanf("%f",&side);
+	area=side*side;
+	printf("\n Area of square : %f",area);
+	getch();
+}
