2b711843a8786ee1c0b416af01733f18167db7c0
tulsibhagat259 <riya.bhagat.259@gmail.com>
2025-08-26 22:14:37 +0530

    daily

---
+++ assiment/Lab/INFORMAT.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("\nA Your Name : Tulsi bhagat ");
+	printf("\nB Your Birth Date : 25/09/2006");
+	printf("\nC Your Age : 19 ");
+	printf("\nD Your Address : ahmedabad ");
+	getch();
+}
+
+
+
+
+
+
