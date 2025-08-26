2bc513f110f5d1f79077f1bf3a26a67196e8da96
tulsibhagat259 <riya.bhagat.259@gmail.com>
2025-08-26 22:17:00 +0530

    daily

---
+++ assiment/Lab/AREA.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	float radius, area, circumference;
+
+	printf("\nEnter the radius of the circle:");
+	scanf("%d",&radius);
+
+	printf("\nArea of tha circle = %.2d",area);
+	printf("\nCircumference of the circle = %.2d",circumference);
+	getch();
+}
