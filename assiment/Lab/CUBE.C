24dfb932e8add42f59d49045e6eda4399b4a185c
tulsibhagat259 <riya.bhagat.259@gmail.com>
2025-08-26 22:18:45 +0530

    daily

---
+++ assiment/Lab/CUBE.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	float side,area;
+	printf("\n Enter the Length of side : ");
+	scanf("%f",&side);
+	area=6*side*side;
+	printf("\n Area of cube : %f",area);
+	getch();
+}
+
