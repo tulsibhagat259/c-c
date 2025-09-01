7a034fe2eea8b4216d6cdf48333df9819995ed4e
tulsi <riya.bhagat.259@gmail.com>
2025-09-01 15:25:47 +0530

    Update FIBONACC.C

--- assiment/FIBONACC.C
+++ assiment/FIBONACC.C
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<conio.h>
 
-void main()
+int main()
 {
-	int n,t1=0,t2=1,nextTerm;
+	int n,i,t1=0,t2=1,nextTerm;
 	printf("\n Enter the number of terms : ");
 	scanf("%d",&n);
 	printf("\n Fibonacci series : ");
-	for (int i=1;i<=n;i++)
+	for (i=1;i<=n;i++)
 	{
 		printf("%d",t1);
 		nextTerm=t1+t2;
 		t1=t2;
 		t2=nextTerm;
 	}
-	getch();
+	return();
 }
